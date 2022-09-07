5fd1d705257907c673c4c66811efeb180b3e31c8
Anirudh Sharma <anirudh03sharma@gmail.com>
2022-09-07 19:08:39 +0530

    Updated problem #5 - Valid Palindrome

--- src/main/java/org/redquark/grind/problems/easy/ValidPalindrome.java
+++ src/main/java/org/redquark/grind/problems/easy/ValidPalindrome.java
@@ -6,6 +6,38 @@ public class ValidPalindrome {
      * Time complexity - O(n)
      * Space complexity - O(n) (String is immutable in Java)
      */
+    public boolean isPalindromeBruteForce(String s) {
+        // Special case
+        if (s == null || s.isEmpty()) {
+            return true;
+        }
+        // Create a new string with only alphanumeric characters
+        StringBuilder alphanumerics = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            if ((c >= 'a' && c <= 'z')
+                    || (c >= 'A' && c <= 'Z')
+                    || (c >= '0' && c <= '9')) {
+                alphanumerics.append(Character.toLowerCase(c));
+            }
+        }
+        // Create a new string with only alphanumerics but in reverse order
+        StringBuilder reverseAlphanumerics = new StringBuilder();
+        for (int i = s.length() - 1; i >= 0; i--) {
+            char c = s.charAt(i);
+            if ((c >= 'a' && c <= 'z')
+                    || (c >= 'A' && c <= 'Z')
+                    || (c >= '0' && c <= '9')) {
+                reverseAlphanumerics.append(Character.toLowerCase(c));
+            }
+        }
+        // Check if both are equal or not
+        return alphanumerics.toString().equals(reverseAlphanumerics.toString());
+    }
+
+    /**
+     * Time complexity - O(n)
+     * Space complexity - O(n) (String is immutable in Java)
+     */
     public boolean isPalindromeSuboptimal(String s) {
         // Special case
         if (s == null || s.isEmpty()) {
@@ -20,7 +52,7 @@ public class ValidPalindrome {
         int right = s.length() - 1;
         while (left < right) {
             // If the characters at left and right pointers are not same,
-            // it means the string is not palinddrome
+            // it means the string is not palindrome
             if (s.charAt(left) != s.charAt(right)) {
                 return false;
             }
--- src/test/java/org/redquark/grind/problems/easy/ValidPalindromeTest.java
+++ src/test/java/org/redquark/grind/problems/easy/ValidPalindromeTest.java
@@ -9,6 +9,18 @@ public class ValidPalindromeTest {
     private final ValidPalindrome validPalindrome = new ValidPalindrome();
 
     @Test
+    public void testIsPalindromeBruteForce() {
+        String s = "A man, a plan, a canal: Panama";
+        assertTrue(validPalindrome.isPalindromeBruteForce(s));
+
+        s = "race a car";
+        assertFalse(validPalindrome.isPalindromeBruteForce(s));
+
+        s = " ";
+        assertTrue(validPalindrome.isPalindromeBruteForce(s));
+    }
+
+    @Test
     public void testIsPalindromeSuboptimal() {
         String s = "A man, a plan, a canal: Panama";
         assertTrue(validPalindrome.isPalindromeSuboptimal(s));
